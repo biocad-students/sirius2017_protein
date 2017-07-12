dc36b416d748e17f6b7b3097c85827bf0c1d80a7
pmsinner <peter.apsbox@yandex.ru>
2017-07-12 16:07:46 +0300

    Align bugfix in CCD

--- algo/CCD.py
+++ algo/CCD.py
@@ -1,17 +1,19 @@
 import numpy as np
 from Bio import PDB as pdb
 from geometry.rotation import rotate_vector, vec_mult_vec, calcnewcord 
-from math import acos, sqrt, atan2, sin, cos
+from math import sqrt
 
 
 def norm(vec): #Нормировка вектора по длине 1
-	return arr(vec)/np.linalg.norm(arr(vec))
+	if sum(list(vec)) > 0:
+		return arr(vec)/np.linalg.norm(arr(vec))
+	return arr(vec)
 
 def arr(vec):  #Конвертация из Vector Biopython в array[3] numpy
 	return np.array(list(vec))
 
 def get_intersection(dot1, dot2, vec): #Нахождение точки пересечения прямой (задаётся dot1 и dot2) и перпендикуляра из vec
-		return np.dot(vec - dot1, norm(dot2-dot1)) * norm(dot2-dot1)
+		return np.dot(vec - dot1, norm(dot2-dot1)) * norm(dot2-dot1) + dot1
 
 def CCD(loop, endpoint, feedback=False):
 	if feedback: #Отладочная информация
@@ -29,17 +31,18 @@ def CCD(loop, endpoint, feedback=False):
 				C_vec = residue['C'].get_vector()
 
 				O_points = np.array([get_intersection(arr(N_vec),arr(CA_vec), arr(loop[-1][x].get_vector())) for x in ['N', 'CA', 'C']])	#Координаты точек пересечения осей и перпедикуляров из атомов последнего residue на них (Oi)
+
 				multipliers = np.array([np.linalg.norm(arr(loop[-1][x].get_vector()) - O_points[i2]) for i2,x in enumerate(['N','CA','C'])]) #|| OiM'i ||
 
 				c_diff = [vec_mult_vec(norm(loop[-1][x].get_vector() - O_points[i2]), norm(CA_vec-N_vec)) for i2, x in enumerate(['N','CA','C'])]	#si
 
 				c_diff = [np.dot(c_diff[i2], arr(endpoint[x].get_vector()) - O_points[i2]) for i2, x in enumerate(['N','CA','C'])]	#si * OiFi
 								
-				c = -2 * sum([multipliers[i] * c_diff[i] for i in range(3)]) #ci = -2 * (si * OiFi)
+				c = 2 * sum([multipliers[i] * c_diff[i] for i in range(3)]) #ci = -2 * (si * OiFi)
 
 				b_diff = [norm(loop[-1][x].get_vector() - O_points[i2]) for i2,x in enumerate(['N','CA','C'])]	# ri
 				b_diff = [np.dot(b_diff[i2], arr(endpoint[x].get_vector()) - O_points[i2]) for i2, x in enumerate(['N','CA','C'])]	#ri * OiFi
-				b = -2 * sum([multipliers[i] * b_diff[i] for i in range(3)]) #bi = -2 * (ri * OiFi)
+				b = 2 * sum([multipliers[i] * b_diff[i] for i in range(3)]) #bi = -2 * (ri * OiFi)
 
 				sqt = sqrt(b**2+c**2)
 
@@ -49,7 +52,8 @@ def CCD(loop, endpoint, feedback=False):
 					for atom in res:
 						
 						coord = atom.get_vector()
-						atom.set_coord(calcnewcord(N_vec, CA_vec, coord, c/sqt,-b/sqt)) 
+						#print(calcnewcord(N_vec, CA_vec, coord, c/sqt,-b/sqt))
+						atom.set_coord(calcnewcord(N_vec, CA_vec, coord, -c/sqt,b/sqt)) 
 
 				N_vec = residue['N'].get_vector()
 				CA_vec= residue['CA'].get_vector()
@@ -63,18 +67,18 @@ def CCD(loop, endpoint, feedback=False):
 
 				c_diff = [np.dot(c_diff[i2], arr(endpoint[x].get_vector()) - O_points[i2]) for i2, x in enumerate(['N','CA','C'])]	#si * OiFi
 								
-				c = -2 * sum([multipliers[i] * c_diff[i] for i in range(3)])	#ci = -2 * (si * OiFi)
+				c = 2 * sum([multipliers[i] * c_diff[i] for i in range(3)])	#ci = -2 * (si * OiFi)
 
 				b_diff = [norm(loop[-1][x].get_vector() - O_points[i2]) for i2,x in enumerate(['N','CA','C'])]	#ri
 				b_diff = [np.dot(b_diff[i2], arr(endpoint[x].get_vector()) - O_points[i2]) for i2, x in enumerate(['N','CA','C'])]	#ri * OiFi
-				b = -2 * sum([multipliers[i] * b_diff[i] for i in range(3)])	#bi = -2 * (ri * OiFi)
+				b = 2 * sum([multipliers[i] * b_diff[i] for i in range(3)])	#bi = -2 * (ri * OiFi)
 
 				sqt = sqrt(b**2+c**2)
 				residues = [[residue['O']]] + loop[i1+1:] #Затем ψ-вращение. Все атомы последующих residue и O текущего residue
 				for res in residues:
 					for atom in res:
 						coord = atom.get_vector()
-						atom.set_coord(calcnewcord(CA_vec, C_vec, coord, c/sqt,-b/sqt))				
+						atom.set_coord(calcnewcord(CA_vec, C_vec, coord, -c/sqt, b/sqt))				
 		N_dist = np.linalg.norm(arr(loop[-1]['N'].get_vector())-arr(endpoint['N'].get_coord()))
 		CA_dist = np.linalg.norm(arr(loop[-1]['CA'].get_vector())-arr(endpoint['CA'].get_coord()))
 		C_dist = np.linalg.norm(arr(loop[-1]['C'].get_vector())-arr(endpoint['C'].get_coord()))
