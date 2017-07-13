8abd90adfd72f4fe23c5a6d56fc5d88c35aa8df8
pmsinner <peter.apsbox@yandex.ru>
2017-07-13 10:37:38 +0300

    Исправлена ошибка с последним residue в петле

--- utils/strstr.py
+++ utils/strstr.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-def get_residue_number(char_number, helis):
-	splt = helis[:char_number+1].split()
+
+def get_residue_number(char_number, loops):
+	splt = loops[:char_number+1].split()
 	return len(splt)-1, len(splt[-1])-1
 
 def dict_ziper(dictionary):
@@ -20,36 +21,33 @@ def maxSubstring(string1, string2, lenner):
 				matrix[i,j] = matrix[i-1,j-1] + 1
 				if matrix[i,j] - lenner > maxpos[2]:
 					maxpos = (i,j,matrix[i,j]- lenner)
-	substring = ''
-	for i in range(maxpos[2]):
-		substring = string1[maxpos[0]-1-i]+ substring
-	return maxpos[0] - maxpos[2], maxpos[1] - maxpos[2], maxpos[2], substring
+	return maxpos[0] - maxpos[2], maxpos[1] - maxpos[2], maxpos[2]
 
-def heliSubstring(to_parse, var_number, hidden_num=None):
+def loopSubstring(to_parse, var_number, hidden_num=None):
 	to_parse = 'SDVVVVTSIMRPAYYYGM'
-	f = open('../sirius_out/helis.txt', 'r')
-	helis = f.read()
+	f = open('../sirius_out/loops.txt', 'r')
+	loops = f.read()
 	f.close()
 	f = open('../sirius_out/ids.txt', 'r')
 	ids = f.read().split()
 	f.close()
 	if hidden_num:
 		index = ids.index(hidden_num)
-		helis = helis.split()
-		del(helis[index])
+		loops = loops.split()
+		del(loops[index])
 		del(ids[index])
-		helis = ' '.join(helis)
+		loops = ' '.join(loops)
 	results=[]
 	for i in range(var_number):
 		parsing = to_parse
 		results.append({})
 		i2 = 0
 		while not all([s == '' for s in parsing.split('%')]):
-			out = maxSubstring(helis,parsing, min(max(i-i2,0), max(max([len(s) for s in parsing.split('%')]) - 2, 0)))
-			nums = get_residue_number(out[0], helis)
+			out = maxSubstring(loops,parsing, min(max(i-i2,0), max(max([len(s) for s in parsing.split('%')]) - 2, 0)))
+			nums = get_residue_number(out[0], loops)
 			results[i][out[1]] = (ids[nums[0]], nums[1], out[2])
 			parsing = parsing[:out[1]] + '%' * out[2] + parsing[out[1]+out[2]:]
 			i2+=1
 		results[i] = dict_ziper(results[i])
 	return results
-print(heliSubstring('SDVVVVTSIMRPAYYYGM', 3))
+
