a6cff2a209a8e1111287e2880d68c3a1bcc8accf
pmsinner <peter.apsbox@yandex.ru>
2017-07-13 11:37:23 +0300

    Извлечение структур из БД

--- utils/strstr.py
+++ utils/strstr.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def get_residue_number(char_number, loops):
+def get_residue_number(char_number, loops):	#Номер  pdb файла по его индексу в regions.txt
 	splt = loops[:char_number+1].split()
 	return len(splt)-1, len(splt[-1])-1
 
@@ -12,26 +12,32 @@ def dict_ziper(dictionary):
 		result.append(dictionary[key])
 	return result
 
-def maxSubstring(string1, string2, lenner):
+def maxSubstring(string1, string2, lenner): #Максимальная общая подстрока string1 и string2
 	maxpos = (0, 0, 0)
 	matrix = np.zeros((len(string1)+1,len(string2)+1), dtype=np.int)
 	for i in range(len(string1)+1):
 		for j in range(len(string2)+1):
 			if min(i,j) > 0 and string1[i-1] == string2[j-1]:
 				matrix[i,j] = matrix[i-1,j-1] + 1
-				if matrix[i,j] - lenner > maxpos[2]:
+				if matrix[i,j] - lenner > maxpos[2]: #Уменьшаем потенциальнуя строку на lenner для вариативности
 					maxpos = (i,j,matrix[i,j]- lenner)
 	return maxpos[0] - maxpos[2], maxpos[1] - maxpos[2], maxpos[2]
 
 def loopSubstring(to_parse, var_number, hidden_num=None):
-	to_parse = 'SDVVVVTSIMRPAYYYGM'
+	'''
+		loopSubstring(to_parse, var_number, hidden_num=None)
+		Возвращает var_number вариантов разбиения to_parce на подстроки из БД петель
+		Возвращает список списков кортежей:
+		loopSubstring() = [[(protId, start, len), (protId, start,len)...],[(protId, start, len), (protId, start,len)...],...]
+		Где protId -- id белка, start -- первый residue в последовательности, len -- длина последовательности
+	'''
 	f = open('../sirius_out/loops.txt', 'r')
 	loops = f.read()
 	f.close()
 	f = open('../sirius_out/ids.txt', 'r')
 	ids = f.read().split()
 	f.close()
-	if hidden_num:
+	if hidden_num:		#Прячем "скрытый номер"
 		index = ids.index(hidden_num)
 		loops = loops.split()
 		del(loops[index])
@@ -42,7 +48,7 @@ def loopSubstring(to_parse, var_number, hidden_num=None):
 		parsing = to_parse
 		results.append({})
 		i2 = 0
-		while not all([s == '' for s in parsing.split('%')]):
+		while not all([s == '' for s in parsing.split('%')]):	#Жадно разбиваем на подстроки
 			out = maxSubstring(loops,parsing, min(max(i-i2,0), max(max([len(s) for s in parsing.split('%')]) - 2, 0)))
 			nums = get_residue_number(out[0], loops)
 			results[i][out[1]] = (ids[nums[0]], nums[1], out[2])
@@ -50,4 +56,4 @@ def loopSubstring(to_parse, var_number, hidden_num=None):
 			i2+=1
 		results[i] = dict_ziper(results[i])
 	return results
-
+#Возвращает список списков (вариантов разбиения) кортежей (каждый из которых имеет)
---
+++ utils/withdraw.py
@@ -0,0 +1,22 @@
+from Bio import PDB as pdb
+
+def regions_to_dict(regions): #Превращает строку файла regions в словарь id:[имена residue]
+	result = {}
+	for string in regions.split('\n'):
+		if string:
+			result[string.split()[0]] = string.split()[1:]
+	return result
+
+def get_residues_by_pos(resList): 
+	'''
+		Принимает список кортежей в формате вывода strstr.loopSubstring, возвращает список списков соответствующих residue
+	'''
+	parser = pdb.PDBParser()
+	residues = []
+	regions = regions_to_dict(open('../sirius_out/regions.txt', 'r').read())
+	for res_tuple in resList:
+		structure = parser.get_structure('strk', '../sirius_out/'+res_tuple[0]+'.pdb')
+		align = sum([ len(s) for s in regions[res_tuple[0]][-2]]) + res_tuple[1]
+		residues.append(list(structure.get_residues())[align:align+res_tuple[2]+1])
+	return residues
+
