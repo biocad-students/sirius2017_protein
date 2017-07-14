015ef1e8fc3fd7e57210d3bf498f68d5d322b81f
Pavel Khariov <swordingo@gmail.com>
2017-07-14 12:01:45 +0300

    Функция получения параметров в отдельном файле

---
+++ params/params.py
@@ -0,0 +1,33 @@
+def varrand(mas, b):
+    def rand(am1, am2, j):
+        Pam = (am1 + ' ' + am2)
+        f = open('text.txt')
+        q = 0
+        arr = []
+        for line in f:
+            string = line
+            if line.find(Pam) != -1:
+                arr.append(string)
+                q += 1
+        p = 1
+        while p <= int(j):
+            r = random.randint(0, q - 1)
+            g = arr[r].split()
+            an = []
+            an.append(g[2])
+            an.append(g[3])
+            an.append(g[4])
+            kolvo.append(an)
+            p += 1
+        f.close()
+    z = 1
+    x = 0
+    itog = [[[1, 2, 3]] * (len(mas) - 1) for o in range(int(b))]
+    while z < len(mas):
+        rand(mas[z - 1], mas[z], int(b))
+        while x < int(b):
+            itog [x] [z - 1] = kolvo[x + (z - 1) * int(b)]
+            x += 1
+        z += 1
+        x = 0
+    return (itog)
