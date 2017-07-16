8525d36add27c2d18af1de8189317f99591b73a2
Pavel Khariov <swordingo@gmail.com>
2017-07-16 16:51:25 +0300

    Функция для гистограммы

---
+++ params/Funk_hist.py
@@ -0,0 +1,24 @@
+def histo(param):
+    f = open('results.txt')
+    a = open('results_hist.txt', 'w')
+    fl = []
+    maxy = 0
+    for line in f:
+        st = line
+        mas = st.split()
+        fl.append(float(mas[1]))
+        if float(mas[1]) > maxy:
+            maxy = float(mas[1])
+    n = 0
+    n = float(n)
+    while n < maxy * 10 + 0.1:
+        k = 0
+        ch = 0
+        while k < len(fl):
+            if float(fl[k]) >= n/10 and float(fl[k]) < n/10 + 0.1:
+                ch += 1
+            k += 1
+        n += param * 10
+        a.write (str(n/10) + ' ' + str(ch) + '\n')
+    f.close()
+    a.close()
