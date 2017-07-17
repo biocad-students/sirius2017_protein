c46946243c5f76ada9d8c8273a318dab753d0163
Vova <vovavelho@gmail.com>
2017-07-17 20:52:19 +0300

    final.py is ready

--- main/final.py
+++ main/final.py
@@ -6,12 +6,13 @@ from params.Funk_hist import *
 if len(sys.argv) > 1:
 	pathToRegions = sys.argv[1] + "regions.txt"
 	pathToSource = sys.argv[1]
+    pathToResults = sys.argv[2]
 else:
 	pathToRegions = "../../../Desktop/sirius_out/regions.txt"
 	pathToSource = "../../../Desktop/sirius_out/"
+    pathToResults = "result/"
 
-pathToRegions = "../../../Desktop/sirius_out/regions.txt"
-pathToResults = "result/"
+#pathToRegions = "../../../Desktop/sirius_out/regions.txt"
 pathToOut = "rmsout.txt"
 
 
